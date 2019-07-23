c5b9c497e6fa4195312bc6c6fa9dc3d5ad223ac6
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-07-23 00:51:55 -0300

    implementando las propiedades para product

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    const PRODUCTO_DISPONIBLE='disponible';
+    const PRODUCTO_NO_DISPONIBLE='no disponible';
+
+    protected $fillable=[
+    	'name',
+    	'description',
+    	'quantity',
+    	'status',
+    	'image',
+    	'seller_id'
+    ];
+
+    public function estaDisponible(){
+    	//retorna true si el producto esta disponbile
+    	return $this->status==self::PRODUCTO_DISPONIBLE;
+    }
+
 }
