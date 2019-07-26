bc48acb399b37761cc47016b47f33d38a3fa751f
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-07-25 23:13:11 -0300

    implementando migracion para products

--- database/migrations/2019_07_22_052215_create_products_table.php
+++ database/migrations/2019_07_22_052215_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,6 +16,17 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
+            $table->string('image');
+            $table->integer('seller_id')->unsigned();
+            //el vendedor-seller es un usuario, no tendremos una tabla seller
+            $table->foreign('seller_id')->references('id')->on('users');
+
+
+
             $table->timestamps();
         });
     }
