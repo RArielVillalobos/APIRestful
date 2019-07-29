c0d2886725d1d56fac5c11f4d27aa75397e55d42
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-07-29 00:11:11 -0300

    54-removiendo un usuario con el metodo destroy de UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -144,5 +144,8 @@ class UserController extends Controller
     public function destroy($id)
     {
         //
+        $user=User::findOrFail($id);
+        $user->delete();
+        return response()->json(['data'=>$user],200);
     }
 }
