3d304f091833e7a4046f206ec194ea0507a1e800
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-08-21 16:02:25 -0300

    91-implementando el metodo index de BuyerCategoryController

---
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BuyerCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        //lista de categorias que ha comprado un determinado comprador
+        $categories=$buyer->transactions()->with('product.categories')
+            ->get()
+            ->pluck('product.categories')
+            ->collapse()
+            ->unique('id')
+            ->values();
+
+        dd($categories);
+        return $this->showAll($categories);
+
+    }
+
+
+}
