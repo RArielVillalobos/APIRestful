6650ddc6c77911cac0400a1ddebc038eb79f9d8f
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-10-19 19:24:24 -0300

    115-actualizando la imagen al editar un producto

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -93,6 +93,11 @@ class SellerProductController extends ApiController
 
             }
         }
+        if($request->hasFile('image')){
+            Storage::delete($product->image);
+            $product->image=$request->image->store('');
+            
+        }
         //si no se modificio la instancia
         if($product->isClean()){
             return $this->errorResponse('Se debe especificar al menos un valor diferente para actualizar',422);
