8b454b135a5d55d674da49d4dc09fc32d8065c5f
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-10-27 16:59:21 -0300

    141-ordenando los resultados usando cualquier atributo

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -29,6 +29,7 @@ trait ApiResponser{
             return $this->successResponse(['data'=>$collection],$code);
         }
         $transformer=$collection->first()->transformer;
+        $collection=$this->sortData($collection);
         $collection= $this->transformData($collection,$transformer);
 
         return $this->successResponse($collection,$code);
@@ -42,6 +43,17 @@ trait ApiResponser{
     }
     protected function showMessage($message,$code=200){
         return $this->successResponse(['data'=>$message],$code);
+
+    }
+    
+    public function sortData(Collection $collection){
+        if(request()->has('sort_by')){
+            $attribute=request()->sort_by;
+            $collection=$collection->sortBy->{$attribute};
+
+
+        }
+        return $collection;
 
     }
 
