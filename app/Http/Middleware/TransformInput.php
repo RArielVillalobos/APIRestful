74e6d92b73149ed7d44b06920e3746098cb22ccb
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-11-03 01:44:11 -0300

    160-implementando la solucion al primer problema usando el middleware que creamos

--- app/Http/Middleware/TransformInput.php
+++ app/Http/Middleware/TransformInput.php
@@ -13,8 +13,22 @@ class TransformInput
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next,$transformer)
     {
+        //vamos a transformar las entradas
+        //hay que ser cuidadoso y solo transformar las entradas y no incluir los query params(los params que vienen en la url)
+        $transformedInput=[];
+        //vamos a recorrer los campos recibimos, unicamente en el cuerpo de la consulta y no como los parametros
+        //esto lo hacemos a traves de $request->all (tiene lo relacionado con la peticion y no con lo que viene en la url)
+
+        foreach ($request->all() as $input=>$value){
+            $transformedInput[$transformer::originalAttribute($input)]=$value;
+
+        }
+
+        //remplazamos la peticion original
+        $request->replace($transformedInput);
+
         return $next($request);
     }
 }
